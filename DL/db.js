7fdde2f700d67075112003f6d0b9e2f7e3cd3841
Yehuda Yeger <yeger921@gmail.com>
2023-06-25 11:22:40 +0300

    fixed config files

--- DL/db.js
+++ DL/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-// const MONGO_URL = process.env.MONGO_URL; THIS IS NOT WORKING 
-const MONGO_URL = 'mongodb+srv://bootcampDB:bootcampDB@clusterbootcamp.d79r1uq.mongodb.net/bootcampDB?retryWrites=true&w=majority'
+const MONGO_URL = process.env.MONGO_URL
+// const MONGO_URL = 'mongodb+srv://bootcampDB:bootcampDB@clusterbootcamp.d79r1uq.mongodb.net/bootcampDB?retryWrites=true&w=majority'
 const connect = async () => {
     try {
         const con = await mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
